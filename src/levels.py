9b34c052913f9f88b1c60c25cff2470ec6dc7690
Jorge Adrian Saghin Dudulea <39440777+Z4na14@users.noreply.github.com>
2023-11-26 11:55:30 +0100

    Collisions over

--- src/levels.py
+++ src/levels.py
@@ -4,12 +4,15 @@ class Platform:
         self.positionY = positionY
         self.width = width
         self.height = height
+
+
 """
     def kick(self):
 """
 
+
 class Screen:
-    def __init__(self, lv: int, platforms: list, pipes : list):
+    def __init__(self, lv: int, platforms: list, pipes: list):
         self.lv = lv
         self.platforms = platforms
         self.pipes = pipes
